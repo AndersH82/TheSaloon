42cb817ef25348fbea465e19901ac8743a0dde01
Anders Holst <andersthereseholst@gmail.com>
2024-02-11 20:32:04 +0000

    Add follows to profile model

--- the_saloon/models.py
+++ the_saloon/models.py
@@ -4,4 +4,7 @@ from django.contrib.auth.models import User
 # Creat a user profile model
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
+    follows = models.ManyToManyField("self",
+         related_name="followed_by",
+         symmetrical=False,
+         blank=True)
