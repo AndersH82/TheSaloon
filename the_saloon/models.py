479e0ab43311fb9e6df0895806af3aacb6e47873
Anders Holst <andersthereseholst@gmail.com>
2024-02-12 08:11:42 +0000

    make user automatic get en profile in models.py

--- the_saloon/models.py
+++ the_saloon/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Creat a user profile model
 class Profile(models.Model):
@@ -11,3 +12,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+# Creat profile new user sign up
+def class_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        # Make user follow themselves
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+
+    post_save.connect(create_profile, sender=User)
