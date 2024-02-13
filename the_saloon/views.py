f71f89dd00c238dc0593f9a0c2e48e7faf94b16e
Anders Holst <andersthereseholst@gmail.com>
2024-02-13 11:20:16 +0000

    added login function and html

--- the_saloon/views.py
+++ the_saloon/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Profile, Shout
 from .forms import ShoutForm
+from django.contrib.auth import authenticate, login, logout
+
 
 def home(request):
     if request.user.is_authenticated:
@@ -48,3 +50,24 @@ def profile(request, pk):
     else:
         messages.success(request, ("You must login to see this page..."))
         return redirect('home')
+
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You are now logged in!"))
+            return redirect('home')
+        else:
+            messages.success(request, ("WROOOONG! Try again!"))
+            return redirect('login')
+
+    else:    
+        return render(request, "login.html", {})
+
+def logout_user(request):
+    pass
