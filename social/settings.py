78ff3818331edd77978868bbf52cc503c59b0c2f
Anders Holst <andersthereseholst@gmail.com>
2024-02-21 12:35:48 +0000

    settings

--- social/settings.py
+++ social/settings.py
@@ -20,13 +20,12 @@ ALLOWED_HOSTS = ['8000-andersh82-thesaloon-vokdu2y0x5e.ws-eu108.gitpod.io','.her
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    
+    'django.contrib.staticfiles',
     'the_saloon',
 ]
 
@@ -38,7 +37,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'social.urls'
@@ -107,13 +105,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'social/static'),
-) 
+STATICFILES_DIRS = [
+    os.path.join(str(BASE_DIR.joinpath('social/static')),)
+] 
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
