e244b1782a42c3ec8c53995b64667017031c5e2a
Anders Holst <andersthereseholst@gmail.com>
2024-02-21 10:41:08 +0000

    changes

--- social/settings.py
+++ social/settings.py
@@ -107,7 +107,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     os.path.join(str(BASE_DIR.joinpath('social/static/')),)
