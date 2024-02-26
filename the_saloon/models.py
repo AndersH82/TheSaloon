5275e9cbae3ae23b6fd5d42ab8f48955c004e190
Anders Holst <andersthereseholst@gmail.com>
2024-02-26 21:53:59 +0000

    fixed pep8 on models.py and test.py

--- the_saloon/models.py
+++ the_saloon/models.py
@@ -2,63 +2,65 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.db.models.signals import post_delete
+
 
 # Shout Model
 class Shout(models.Model):
     user = models.ForeignKey(
         User, related_name="shouts",
         on_delete=models.DO_NOTHING
-        )
+    )
     body = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name="shout_like", blank=True)
 
     def number_of_likes(self):
-		    return self.likes.count()
+        return self.likes.count()
 
     def __str__(self):
-        return(
+        return (
             f"{self.user} "
             f"({self.created_at:%Y-%m-%d %H:%M}): "
             f"{self.body}..."
-            )
+        )
 
 
 # Profile model
 class Profile(models.Model):
-        user = models.OneToOneField(User, on_delete=models.CASCADE)
-        follows = models.ManyToManyField("self", 
-            related_name="followed_by",
-            symmetrical=False,
-            blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField("self",
+                                     related_name="followed_by",
+                                     symmetrical=False, blank=True)
 
+    date_modified = models.DateTimeField(User, auto_now=True)
+    profile_image = models.ImageField(
+        null=True, blank=True, upload_to="images/")
+    profile_bio = models.CharField(null=True, blank=True, max_length=500)
+    facebook_link = models.CharField(null=True, blank=True, max_length=100)
+    instagram_link = models.CharField(
+        null=True, blank=True, max_length=100)
+    linkedin_link = models.CharField(null=True, blank=True, max_length=100)
+    youtube_link = models.CharField(null=True, blank=True, max_length=100)
+    x_link = models.CharField(null=True, blank=True, max_length=100)
 
-        date_modified = models.DateTimeField(User, auto_now=True)
-        profile_image = models.ImageField(null=True, blank=True, upload_to="images/")
-        profile_bio = models.CharField(null=True, blank=True, max_length=500)
-        facebook_link = models.CharField(null=True, blank=True, max_length=100)
-        instagram_link = models.CharField(null=True, blank=True, max_length=100)
-        linkedin_link = models.CharField(null=True, blank=True, max_length=100)
-        youtube_link = models.CharField(null=True, blank=True, max_length=100)
-        x_link = models.CharField(null=True, blank=True, max_length=100)
-
-        def __str__(self):
-                return self.user.username
-       
+    def __str__(self):
+        return self.user.username
 
 # Creat profile new user sign up
+
+
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
-	if created:
-		user_profile = Profile(user=instance)
-		user_profile.save()
-		# Have the user follow themselves
-		user_profile.follows.set([instance.profile.id])
-		user_profile.save()
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        # Have the user follow themselves
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
 
 # Gallary for users images
+
+
 class UploadedImage(models.Model):
     image = models.ImageField(upload_to='uploads/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
-
--- the_saloon/tests.py
+++ the_saloon/tests.py
@@ -1,12 +1,13 @@
 from django.test import TestCase, Client
 from django.urls import reverse, resolve
 from django.contrib.auth.models import User
-from the_saloon.views import home, profile_list, login_user, update_user, upload_image
-from the_saloon.models import Profile, Shout
-from django.contrib.auth.models import User
-from .forms import ProfilePicForm, ShoutForm, SignUpForm
+from the_saloon.views import home, profile_list, login_user, update_user
+from the_saloon.views import upload_image
+from the_saloon.models import Shout
+from .forms import ProfilePicForm, SignUpForm
 
 # Test Urls
+
 
 class TestUrls(TestCase):
 
@@ -15,24 +16,20 @@ class TestUrls(TestCase):
         print(resolve(url))
         self.assertEqual(resolve(url).func, home)
 
-
     def test_creat_url_is_resolves(self):
         url = reverse('profile_list')
         print(resolve(url))
         self.assertEqual(resolve(url).func, profile_list)
-    
 
     def test_login_url_is_resolves(self):
         url = reverse('login')
         print(resolve(url))
         self.assertEqual(resolve(url).func, login_user)
 
-
     def test_update_user_url_is_resolves(self):
         url = reverse('update_user')
         print(resolve(url))
         self.assertEqual(resolve(url).func, update_user)
-
 
     def test_upload_image_url_is_resolves(self):
         url = reverse('upload_image')
@@ -41,30 +38,28 @@ class TestUrls(TestCase):
 
 # Test Views
 
+
 class ProfileViewTest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_user(username='user_id=pk', password='password')
+        self.user = User.objects.create_user(
+            username='user_id=pk', password='password')
 
     def test_profile_view(self):
         self.client.login(username='user_id=pk', password='password')
-
-        response = self.client.get('/profile/')
-    
-    def test_profile_views_requires_login(self):
-
-        response = self.client.get('/login/')
 
 
 class LoginUserTest(TestCase):
 
     def test_login_success(self):
-        response = self.client.post(reverse('login'), {'username': 'user', 'password': 'pass'})
+        response = self.client.post(
+            reverse('login'), {'username': 'user', 'password': 'pass'})
         self.assertEqual(response.status_code, 302)
         self.assertTrue(response.url == reverse('login'))
 
     def test_login_fail(self):
-        response = self.client.post(reverse('login'), {'username': 'user', 'password': 'password'})
+        response = self.client.post(
+            reverse('login'), {'username': 'user', 'password': 'password'})
         self.assertEqual(response.status_code, 302)
         self.assertTrue(response.url == reverse('login'))
 
@@ -74,19 +69,16 @@ class LoginUserTest(TestCase):
 
 # Test Models
 
+
 class ShoutModelTest(TestCase):
-    
+
     def setUpTestData():
-        test_user = User.objects.create_user(username="User", password='password')
+        test_user = User.objects.create_user(
+            username="User", password='password')
         Shout.objects.create(user=test_user, body='Test shout body')
 
     def test_number_of_likes(self):
         shout = Shout.objects.get(id=1)
-        self.assertEqual(shout.number_of_likes(), 0)
-
-    def test_number_of_likes_after_liking(self):
-        shout = Shout.objects.get(id=1)
-        test_user = User.objects.create_user(username='user', password='password')
         self.assertEqual(shout.number_of_likes(), 0)
 
 # Test Forms
@@ -96,11 +88,13 @@ class ProfilePicFormTest(TestCase):
 
     def test_form_fields(self):
         form = ProfilePicForm()
-        self.assertSequenceEqual(list(form.fields.keys()), ['profile_image', 'profile_bio', 'facebook_link', 'instagram_link', 'linkedin_link', 'youtube_link', 'x_link'])
+        self.assertSequenceEqual(list(form.fields.keys()), [
+                                 'profile_image', 'profile_bio',
+                                 'facebook_link', 'instagram_link',
+                                 'linkedin_link', 'youtube_link', 'x_link'])
 
-    
     def test_form_rendering(self):
-    
+
         form = ProfilePicForm()
         rendered = form.as_p()
         self.assertIn('Profile Picture', rendered)
@@ -111,6 +105,7 @@ class ProfilePicFormTest(TestCase):
         self.assertIn('Youtube Link', rendered)
         self.assertIn('X Link', rendered)
 
+
 class ShoutFormTest(TestCase):
 
     def setUp(self):
@@ -118,6 +113,7 @@ class ShoutFormTest(TestCase):
 
     def tearDown(self):
         Shout.objects.all().delete()
+
 
 class SignUpFormTest(TestCase):
     def setUp(self):
@@ -144,5 +140,3 @@ class SignUpFormTest(TestCase):
             'password2': 'testpassword',
         })
         self.assertFalse(form.is_valid())
-
-    
